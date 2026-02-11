ee9a95cf90ca6a9cc777696434e18a534a1863d0
Akkun716 <adon.anglon@gmail.com>
2026-02-10 16:20:24 -0800

    Updated comment

--- Button_Baremetal/src/button.cpp
+++ Button_Baremetal/src/button.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
-
+// Register Macros
+#define MYPORTD *( (volatile byte *) 0x2B)
+#define MYDDRD *( (volatile byte *) 0x2A)
+#define MYPIND *( (volatile byte *) 0x29)
+// Digital Input Macros
 #define BUTTON (1 << PIND2)
+// Digital Output Macros
 #define LED (1 << PIND3)
 
 void setup() {
     DDRD |= LED;
+    // Pullup internal resistor to high
     PORTD |= BUTTON;
 }
 
